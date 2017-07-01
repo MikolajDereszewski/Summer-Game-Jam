e216bbec77c9a9784db7ecccc656f69c6e3e5d97
Mikołaj Dereszewski <mikolaj.dereszewski@gmail.com>
2017-07-01 12:29:50 +0200

    Added basic game join

---
+++ Assets/!CreatedAssets/Scripts/Menu/MainMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    [SerializeField]
+    private string _levelToLoad;
+
+    public void OnButtonClicked(int playerType)
+    {
+        PlayerPrefs.SetInt("LOCAL_PLAYER_TYPE", playerType);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(_levelToLoad);
+    }
+}
---
+++ Assets/!CreatedAssets/Scripts/NetCode/NetworkManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum PlayerType
+{
+    Captain = 0,
+    Shooter = 1
+}
+
+public class NetworkManager : MonoBehaviour {
+
+    [SerializeField]
+    private string _gameVersion = "0.0.1";
+    [SerializeField]
+    private GameManager _gameManager = null;
+
+    [SerializeField]
+    private GameObject _captainPrefab = null;
+    [SerializeField]
+    private GameObject _shooterPrefab = null;
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(_gameVersion);
+        //JoinRoom((PlayerPrefs.GetInt("LOCAL_PLAYER_TYPE") == 0) ? PlayerType.Captain : PlayerType.Shooter);
+    }
+
+    private void JoinRoom(PlayerType type)
+    {
+        switch(type)
+        {
+            case PlayerType.Captain:
+                RoomOptions roomOptions = new RoomOptions() { isVisible = true, maxPlayers = 2 };
+                PhotonNetwork.CreateRoom("GAME_ROOM", roomOptions, TypedLobby.Default);
+                break;
+            case PlayerType.Shooter:
+                PhotonNetwork.JoinRoom("GAME_ROOM");
+                break;
+        }
+        AddPlayer(type);
+    }
+
+    void OnJoinedLobby()
+    {
+        JoinRoom((PlayerPrefs.GetInt("LOCAL_PLAYER_TYPE") == 0) ? PlayerType.Captain : PlayerType.Shooter);
+        Debug.Log("Joined to lobby!");
+    }
+
+    private void AddPlayer(PlayerType type)
+    {
+        _gameManager.StartGame();
+    }
+}
